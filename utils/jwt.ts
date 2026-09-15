109aa44b7f9d2c7f4ffd051d461f1bc35029839e
Sazzad Hasan <sazzadhasan313@gmail.com>
2026-09-15 21:14:49 +0600

    fix error

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -12,10 +12,7 @@ const verifyToken = (token:string, secret: string)=>{
         }
     } catch (error: any) {
         console.log(`Token verification failed ${error}`)
-        return{
-            success: false,
-            error: error.message
-        }
+      
     }
 }
 export const jwtUtils = {
